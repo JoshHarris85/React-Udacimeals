3bcb51a7c44518c54990f187eb4d627ed3c8c004
Josh Harris <joshua.harris1085@gmail.com>
2017-09-25 00:56:43 -0500

    impelement initial state holding with react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
         Hello World
       </div>
     )
   }
-
 }
 
-export default App
+function mapStateToProps ({ food, calendar }) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? food[calendar[day][meal]]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,23 @@
+import { combineReducers } from 'redux'
+
 import {
   ADD_RECIPE,
-  REMOVE_FROM_CALENDAR
+  REMOVE_FROM_CALENDAR,
 } from '../actions'
+
+function food (state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE :
+      const { recipe } = action
+
+      return {
+        ...state,
+        [recipe.label]: recipe,
+      }
+    default :
+      return state
+  }
+}
 
 const initialCalendarState = {
   sunday: {
@@ -41,28 +57,32 @@ const initialCalendarState = {
   },
 }
 
-function calander(state = initialCalendarState, action) {
+function calendar (state = initialCalendarState, action) {
   const { day, recipe, meal } = action
-  switch(action.type) {
-    case ADD_RECIPE:
-    return {
+
+  switch (action.type) {
+    case ADD_RECIPE :
+      return {
         ...state,
         [day]: {
           ...state[day],
           [meal]: recipe.label,
         }
       }
-    case REMOVE_FROM_CALENDAR:
-    return {
+    case REMOVE_FROM_CALENDAR :
+      return {
         ...state,
         [day]: {
           ...state[day],
           [meal]: null,
         }
       }
-    default:
-      return state;
+    default :
+      return state
   }
 }
 
-export default calander
+export default combineReducers({
+  food,
+  calendar,
+})
